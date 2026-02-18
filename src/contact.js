3854d2448e7a3dcf9674e5f1d40cdfbd940428b6
Rafael da Silva Lima <ryukiytt@gmail.com>
2026-02-18 16:24:04 -0300

    Add email icon image to the project
    - Introduced a new PNG image file for the email icon located at src/images/email.png.

---
+++ src/contact.js
@@ -0,0 +1,5 @@
+const redes = [
+    {name: "Instagram", link: "https://www.instagram.com"},
+    {name: "Whatsapp", link: "https://www.whatsapp.com"},
+    {name: "Email", link: "https://workspace.google.com/intl/pt-BR/gmail/"}
+]
